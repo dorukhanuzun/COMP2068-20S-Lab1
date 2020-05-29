81122e68e9c8f48d580a70cfcbe4b55220b61c4e
Dorukhan Uzun <dorukhanuzun@MacBook-Pro.local>
2020-05-28 23:09:36 -0400

    JavaScript Lab 1

---
+++ lab1.js
@@ -0,0 +1,76 @@
+/*
+  Full Name: Dorukhan Uzun
+  Student ID: 200427534
+
+  Instructions:
+    Follow the steps below. If you have any questions
+    please feel free to use Slack to communicate. You
+    are allowed to discuss the lab with other students
+    but you ARE NOT permitted to copy/paste or share
+    code.
+  
+  Rubric:
+    Steps: 11.5 points
+    Code Format: 10 points
+    Github Link: 10 points
+
+  NOTE: The code format and Github link marks are
+    only awarded if the steps have been completed.
+    (Code formatting is the visual structure of
+      you code. I expect that you have used proper
+      tabbing of nested structures so code readability
+      is adequate.)
+*/
+
+/*
+  Step 1 (2.5 points):
+    Create an array that contains 4 people objects.
+    Each object must have at least 2 properties.
+*/
+const people = [
+    {
+        name: 'Dorukhan',
+        age : 25,
+        nationality : 'Turkey'
+    },
+    {
+        name: 'Saadet',
+        age: 40,
+        nationality : 'Turkey',
+    },
+    {
+        name: 'Seker',
+        age: 3,
+        nationality : 'Taiwan',
+    },
+    {
+        name: 'Shaun',
+        age: 41,
+        nationality : 'Canada',
+    }
+]
+
+
+/*
+  Step 2 (6 points):
+    Create an arrow function that accepts
+    a destructured object as an argument. In the
+    body of your function, console.log a string
+    template utilizing the properties you
+    destructured.
+*/
+
+const showInConsole = ({name, age, nationality}) => {
+    console.log(`My name is ${name} and my age is ${age} and I am from ${nationality}`);
+}
+
+/*
+  Step 3 (3 points):
+    Loop over your array using a for/of loop.
+    Call your arrow function passing the array item
+    as an argument.
+*/
+
+for(let person of people){
+    showInConsole(person);
+}
